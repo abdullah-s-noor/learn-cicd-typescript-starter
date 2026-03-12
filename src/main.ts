ab5b0c402c8691479033d86afe5d0c31060a6e84
abdullah-s-noor <abdullah.s.noor04@gmail.com>
2026-03-12 23:59:54 +0200

    add eslint check to CI style job

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,10 @@ if (db) {
 }
 
 v1Router.get("/healthz", handlerReadiness);
-
+function unused() {
+  // this function does nothing
+  // and is called nowhere
+}
 app.use("/v1", v1Router);
 
 app.listen(config.api.port, () => {
